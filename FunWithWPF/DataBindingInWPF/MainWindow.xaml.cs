d9328f0593bf3f1e537f8347bf60b5948176213f
deepthakannan <deepthakannan@gmail.com>
2014-12-13 12:18:44 -0500

    Main

---
+++ FunWithWPF/DataBindingInWPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingInWPF
+{
+	class FullContext
+	{
+		public List<KeyValuePair<string, string>> Items { get; set; }
+	}
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = new FullContext() { Items = new List<KeyValuePair<string, string>>() { new KeyValuePair<string, string>("Deeptha", "Kannan"), new KeyValuePair<string, string>("Deeptha13424", "Kannan234242") } };
+			textBox1.DataContext = new TabControl() { TabStripPlacement = Dock.Right };
+		}
+	}
+}
---
+++ FunWithWPF/DataBindingInWPF/WeirdTextBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DataBindingInWPF
+{
+	public class WeirdTextBox : TextBox
+	{
+		public WeirdTextBox()
+		{
+			this.TextChanged += WeirdTextBox_TextChanged;
+		}
+
+		void WeirdTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (ActWeird)
+			{
+				this.Background = this.Background == Brushes.White ? Brushes.Tomato : Brushes.White;
+			}
+			else
+				this.Background = Brushes.White;
+		}
+		public static DependencyProperty ActWeirdProperty = DependencyProperty.Register("ActWeird", typeof(bool), typeof(WeirdTextBox), new FrameworkPropertyMetadata(new PropertyChangedCallback(PropertyChangedCallback)), null);
+
+		[System.ComponentModel.Description("ActWeird")]
+		[System.ComponentModel.Category("ActWeird Category")]
+		[System.ComponentModel.Browsable(true)]
+		[System.ComponentModel.DesignerSerializationVisibility(System.ComponentModel.DesignerSerializationVisibility.Visible)]
+		public bool ActWeird
+		{
+			get
+			{
+				return ((bool)(base.GetValue(WeirdTextBox.ActWeirdProperty)));
+			}
+			set
+			{
+				base.SetValue(WeirdTextBox.ActWeirdProperty, value);
+			}
+		}
+
+		public static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+		}
+	}
+}
