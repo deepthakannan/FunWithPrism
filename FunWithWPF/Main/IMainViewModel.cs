9bd27481454e0eda56e894c81f88a460cf7912e2
deepthakannan <deepthakannan@gmail.com>
2014-12-13 21:14:54 -0500

    new files

---
+++ FunWithWPF/Main/IMainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            LaunchNewSetupWizardCommmand = new SetupNewCommand(this);
+        }
+        internal void LaunchNewSetupWizard()
+        {
+            SetupNewWizard wizard = new SetupNewWizard();
+            wizard.Show();
+        }
+
+        public SetupNewCommand LaunchNewSetupWizardCommmand { get; set; }
+    }
+}
